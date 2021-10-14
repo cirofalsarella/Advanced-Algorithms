6a2ac8305322e203a74434d864e73575055e6a1d
Ciro Falsarella <cirofalsarella@gmail.com>
2021-10-13 22:12:34 -0300

    correção pra reduzir complexidade

--- DivideAndConquer/subArray.cpp
+++ DivideAndConquer/subArray.cpp
@@ -1,30 +1,39 @@
 #include <iostream>
 #include <cstring>
+
 #include <set>
+#include <algorithm>
 
 using namespace :: std;
 
 int v[50010];
 
+
 int calculate(int bot, int top, int min) {
     return (top - bot + 1) * min;
 }
 
-int subarray(int bot, int top) {
-    if (bot >= top) return v[bot];
+int subarray(set<pair<int,int>> &a, int bot, int top) {
+    if (a.size() == 0) return 0;
 
-    // Break the array on the lowest value
-    int min_pos = bot;
-    for (int i=bot+1; i<=top; i++) {
-        if (v[i] < v[min_pos])  min_pos = i;
+    // The lowest value is the min (take and remove)
+    pair<int,int> min = *(a.begin());
+    a.erase({min.first, min.second});
+
+    // Break the array
+    set<pair<int, int>> left;
+    for (int i=bot; i < min.second; i++) {
+        pair<int, int> aux = {v[i], i};
+        left.insert(aux);
+        a.erase(aux);
     }
 
     // Call subarray for both sides
-    int down = subarray(bot, min_pos-1);
-    int up = subarray(min_pos+1, top);
+    int down = subarray(left, bot, min.second-1);
+    int up = subarray(a, min.second+1, top);
 
     // Calculate value from current array
-    int full = calculate(bot, top, v[min_pos]);
+    int full = calculate(bot, top, min.first);
 
     // Return the biggest value from the three
     return max(full, max(up, down));
@@ -32,11 +41,14 @@ int subarray(int bot, int top) {
 
 int main() {
     int n;  cin >> n;
+    
+    set<pair<int, int>> a;
     for (int i=0; i<n; i++) {
         cin >> v[i];
+        a.insert({v[i], i});
     }
 
-    cout << subarray(0, n-1);
+    cout << subarray(a, 0, n-1);
 
     return 0;
 }
